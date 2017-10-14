893174c4ea04a31a0ad5dd0a79b6347044dd9202
Milena Sapunova <milena.sapunova@gmail.com>
2017-10-14 23:30:01 +0300

    Add fields delete and modified for user

--- MilenaSapunova.TerminateConracts/MilenaSapunova.Terminate.Model/User.cs
+++ MilenaSapunova.TerminateConracts/MilenaSapunova.Terminate.Model/User.cs
@@ -1,12 +1,28 @@
-﻿using System.Security.Claims;
+﻿using System;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using MilenaSapunova.Terminate.Model.Contracts;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MilenaSapunova.Terminate.Data.Models
 {
-    public class User : IdentityUser
+    public class User : IdentityUser, IDeletable, IAuditable
     {
+        [Index]
+        public bool IsDeleted { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime? DeletedOn { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime CreatedOn { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime? ModifiedOn { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<User> manager)
         {
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
