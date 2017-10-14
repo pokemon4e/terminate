c068e0121b41c22d6cbe467cd6cc2b19bc818670
Milena Sapunova <milena.sapunova@gmail.com>
2017-10-14 23:16:31 +0300

    Add audit info for all data models

--- MilenaSapunova.TerminateConracts/MilenaSapunova.Terminate.Data/MsSqlDbContext.cs
+++ MilenaSapunova.TerminateConracts/MilenaSapunova.Terminate.Data/MsSqlDbContext.cs
@@ -1,5 +1,11 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
 using MilenaSapunova.Terminate.Data.Models;
+using MilenaSapunova.Terminate.Model.Contracts;
+using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MilenaSapunova.TerminateConracts.Data.Models
 {
@@ -9,6 +15,37 @@ namespace MilenaSapunova.TerminateConracts.Data.Models
         {
         }
 
+        public override int SaveChanges()
+        {
+            this.ApplyAuditInfoRules();
+            return base.SaveChanges();
+        }
+
+        private void ApplyAuditInfoRules()
+        {
+            foreach (var entry in
+                this.ChangeTracker.Entries()
+                    .Where(
+                        e =>
+                        e.Entity is IAuditable && ((e.State == EntityState.Added) || (e.State == EntityState.Modified))))
+            {
+                var entity = (IAuditable)entry.Entity;
+                if (entry.State == EntityState.Added && entity.CreatedOn == default(DateTime))
+                {
+                    entity.CreatedOn = DateTime.Now;
+                }
+                else
+                {
+                    entity.ModifiedOn = DateTime.Now;
+                }
+            }
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken)
+        {
+            this.ApplyAuditInfoRules();
+            return base.SaveChangesAsync(cancellationToken);
+        }
 
         public static MsSqlDbContext Create()
         {
