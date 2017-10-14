07a92e045bcd676ba7906813dcb21f9bee870aed
Milena Sapunova <milena.sapunova@gmail.com>
2017-10-14 23:52:15 +0300

    Add migration

---
+++ MilenaSapunova.TerminateConracts/MilenaSapunova.TerminateContracts.Data/Migrations/201710142050142_Add User fields.cs
@@ -0,0 +1,26 @@
+namespace MilenaSapunova.Terminate.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddUserfields : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.AspNetUsers", "IsDeleted", c => c.Boolean(nullable: false));
+            AddColumn("dbo.AspNetUsers", "DeletedOn", c => c.DateTime());
+            AddColumn("dbo.AspNetUsers", "CreatedOn", c => c.DateTime(nullable: false));
+            AddColumn("dbo.AspNetUsers", "ModifiedOn", c => c.DateTime());
+            CreateIndex("dbo.AspNetUsers", "IsDeleted");
+        }
+        
+        public override void Down()
+        {
+            DropIndex("dbo.AspNetUsers", new[] { "IsDeleted" });
+            DropColumn("dbo.AspNetUsers", "ModifiedOn");
+            DropColumn("dbo.AspNetUsers", "CreatedOn");
+            DropColumn("dbo.AspNetUsers", "DeletedOn");
+            DropColumn("dbo.AspNetUsers", "IsDeleted");
+        }
+    }
+}
