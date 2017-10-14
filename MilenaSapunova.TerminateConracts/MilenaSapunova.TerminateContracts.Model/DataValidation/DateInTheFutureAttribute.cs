d759105af0c7b4f1ec4bbf04b7605ffaf2627fc1
Milena Sapunova <milena.sapunova@gmail.com>
2017-10-15 01:43:00 +0300

    Add data validation

---
+++ MilenaSapunova.TerminateConracts/MilenaSapunova.TerminateContracts.Model/DataValidation/DateInTheFutureAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MilenaSapunova.TerminateContracts.Model.DataValidation
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+    public class DateInTheFutureAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext context)
+        {
+            var futureDate = value as DateTime?;
+            var memberNames = new List<string>() { context.MemberName };
+
+            if (futureDate != null)
+            {
+                if (!(futureDate.Value.Date > DateTime.UtcNow.Date))
+                {
+                    return new ValidationResult("Date must be in the future.", memberNames);
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
---
+++ MilenaSapunova.TerminateConracts/MilenaSapunova.TerminateContracts.Model/DataValidation/ModelsConstraints.cs
@@ -0,0 +1,11 @@
+﻿namespace MilenaSapunova.TerminateContracts.Model.DataValidation
+{
+    public static class ModelsConstraints
+    {
+        public const int ContractTitleMinLength = 3;
+        public const int ContractTitleMaxLength = 30;
+
+        public const int ContractNumberMinLength = 3;
+        public const int ContractNumberMaxLength = 30;
+    }
+}
