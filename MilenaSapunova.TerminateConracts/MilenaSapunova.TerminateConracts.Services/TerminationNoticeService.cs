a8851840ee298fd33490a2565e798d9585336bbd
Milena Sapunova <milena.sapunova@gmail.com>
2017-10-15 10:57:45 +0300

    Add service project

---
+++ MilenaSapunova.TerminateConracts/MilenaSapunova.TerminateConracts.Services/TerminationNoticeService.cs
@@ -0,0 +1,21 @@
+﻿using MilenaSapunova.TerminateContracts.Data.Repositories;
+using MilenaSapunova.TerminateContracts.Model;
+using System.Linq;
+
+namespace MilenaSapunova.TerminateConracts.Services
+{
+    public class TerminationNoticeService
+    {
+        private readonly IEfRepository<TerminationNotice> terminationNoticeRepo;
+
+        public TerminationNoticeService(IEfRepository<TerminationNotice> terminationNoticeRepo)
+        {
+            this.terminationNoticeRepo = terminationNoticeRepo;
+        }
+
+        public IQueryable<TerminationNotice> GetAll()
+        {
+            return this.terminationNoticeRepo.All;
+        }
+    }
+}
