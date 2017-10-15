8999acd065a8081a8541ca240b794abe8e599c56
Milena Sapunova <milena.sapunova@gmail.com>
2017-10-15 03:00:28 +0300

    Add company data model

---
+++ MilenaSapunova.TerminateConracts/MilenaSapunova.TerminateContracts.Model/Company.cs
@@ -0,0 +1,13 @@
+﻿using MilenaSapunova.Terminate.Model.Abstract;
+
+namespace MilenaSapunova.TerminateContracts.Model
+{
+    public class Company : DataModel
+    {
+        public string Name { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
+    }
+}
