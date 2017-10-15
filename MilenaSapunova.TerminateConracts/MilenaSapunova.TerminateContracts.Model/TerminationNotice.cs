ec816cfd3cc1fcf36f3981494a12220197370abf
Milena Sapunova <milena.sapunova@gmail.com>
2017-10-15 05:12:08 +0300

    Add validation for termination notice data model

--- MilenaSapunova.TerminateConracts/MilenaSapunova.TerminateContracts.Model/TerminationNotice.cs
+++ MilenaSapunova.TerminateConracts/MilenaSapunova.TerminateContracts.Model/TerminationNotice.cs
@@ -1,9 +1,12 @@
 ﻿using MilenaSapunova.TerminateContracts.Model.Abstract;
+using MilenaSapunova.TerminateContracts.Model.DataValidation;
+using System.ComponentModel.DataAnnotations;
 
 namespace MilenaSapunova.TerminateContracts.Model
 {
     public class TerminationNotice : DataModel
     {
+        [MaxLength(ModelsConstraints.ContentMaxLength), MinLength(ModelsConstraints.ContentMinLength)]
         public string Content { get; set; }
 
         public virtual Company Company { get; set; }
