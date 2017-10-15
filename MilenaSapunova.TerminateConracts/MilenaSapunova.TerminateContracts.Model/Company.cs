0b039069acf66a222cd1370a8398431e50190a5f
Milena Sapunova <milena.sapunova@gmail.com>
2017-10-15 03:09:44 +0300

    Add validation for company data model

--- MilenaSapunova.TerminateConracts/MilenaSapunova.TerminateContracts.Model/Company.cs
+++ MilenaSapunova.TerminateConracts/MilenaSapunova.TerminateContracts.Model/Company.cs
@@ -1,12 +1,16 @@
 ﻿using MilenaSapunova.Terminate.Model.Abstract;
+using MilenaSapunova.TerminateContracts.Model.DataValidation;
+using System.ComponentModel.DataAnnotations;
 
 namespace MilenaSapunova.TerminateContracts.Model
 {
     public class Company : DataModel
     {
+        [MaxLength(ModelsConstraints.NameMaxLenght), MinLength(ModelsConstraints.NameMinLenght)]
         public string Name { get; set; }
 
-        public string Phone { get; set; }
+        [MaxLength(ModelsConstraints.PhoneNumberMaxLength), MinLength(ModelsConstraints.PhoneNumberMinLength)]
+        public string PhoneNumber { get; set; }
 
         public string Email { get; set; }
     }
--- MilenaSapunova.TerminateConracts/MilenaSapunova.TerminateContracts.Model/DataValidation/ModelsConstraints.cs
+++ MilenaSapunova.TerminateConracts/MilenaSapunova.TerminateContracts.Model/DataValidation/ModelsConstraints.cs
@@ -2,10 +2,18 @@
 {
     public static class ModelsConstraints
     {
+
+        public const int NameMinLenght = 2;
+        public const int NameMaxLenght = 50;
+
         public const int ContractTitleMinLength = 3;
         public const int ContractTitleMaxLength = 30;
 
         public const int ContractNumberMinLength = 3;
         public const int ContractNumberMaxLength = 30;
+
+        public const string PhoneNumberContents = @"^00[0-9]{6,15}$";
+        public const int PhoneNumberMinLength = 8;
+        public const int PhoneNumberMaxLength = 17;
     }
 }
